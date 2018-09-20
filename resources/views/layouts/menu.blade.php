105476a03e0f7816bc9c3d17751f824530672dee
jose.haro <jose.haro@alumnos.udg.mx>
2018-09-20 17:26:02 -0400

    Modificaciones vistas

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,7 +2,7 @@
   <li><a class="app-menu__item" href="index.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Materias</span></a></li>
   <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Formulario</span><i class="treeview-indicator fa fa-angle-right"></i></a>
     <ul class="treeview-menu">
-      <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Formulario Ejemplo 1</a></li>
+      <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Formulario Ejemplo </a></li>
       <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Formulario Registro Alumno</a></li>
     </ul>
   </li>
